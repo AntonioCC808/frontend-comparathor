425dc75029aebea0c0d3714838621d8945558bc0
cuadradot <cuadradot@predictia.es>
2025-01-17 19:46:24 +0100

    problems solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import ts from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), ts()],
-	define: {
-		'import.meta.env.PACKAGE_VERSION': JSON.stringify(
-			process.env.npm_package_version
-		),
-	},
-});
+  plugins: [react()],
+})
